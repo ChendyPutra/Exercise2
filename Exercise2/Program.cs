2e916d2876cba8aab9d61e71074d1b13de2d3685
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-11-02 16:42:33 +0700

    add new variable

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -9,12 +9,14 @@ namespace Exercise2
     internal class Program
     {
         
-        static void Main(int[] arr, int i, int j)
+        static void Main(int[] arr, int i,int k, int j, int low, int mid, int high, int eol,int num,int pos)
         {
             int temp = arr[i];
             arr[j] = arr[i - 1];
             arr[j + 1] = temp;
-
+            j = (mid + 1);
+            i = low;
+            k = low
         }
     }
 }
